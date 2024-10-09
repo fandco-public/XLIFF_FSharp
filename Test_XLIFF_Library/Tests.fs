b6483f82daf41c1572a5833800f32fb604ca3150
Richard Freytag <rfreytag@freytag.us>
2024-10-09 14:09:12 -0400

    XLIFF methods mocked and a start on the tests.   TESTS PASS

--- Test_XLIFF_Library/Tests.fs
+++ Test_XLIFF_Library/Tests.fs
@@ -4,19 +4,113 @@ open System
 open Xunit
 open XLIFF_Library.XLIFF_v1_2
 
-//[<Fact>]
-//let ``My test`` () =
-//    Assert.True(true)
+let xliff_samples = [ 
+                      @"Samples\OpenPasswordGenerator.ar-SA.xlf";
+                      @"Samples\OpenPasswordGenerator.de-DE.xlf";
+                      @"Samples\OpenPasswordGenerator.fr-FR.xlf";
+                      @"Samples\OpenPasswordGenerator.gu-IN.xlf";
+                      @"Samples\OpenPasswordGenerator.ja-JP.xlf";
+                      @"Samples\OpenPasswordGenerator.nl-NL.xlf";
+                      @"Samples\OpenPasswordGenerator.ru-RU.xlf";
+                      @"Samples\Sample_AlmostEverything_1.2_strict.xlf"; 
+                      @"Samples\Sample_AlmostEverything_1.2_transitional.xlf"
+                      ]
 
+// TEST XLIFF_v1_2_Class construction
 [<Fact>]
-let ``Hello method should not throw an exception`` () =
+let ``XLIFF_v1_2_Class construction`` () =
     let instance = XLIFF_v1_2_Class()
-    let action = fun () -> instance.Hello("World")
-    action    
+    Assert.False(instance.ready)
 
-[<Fact>]
-let ``ThrowsException method should throw an exception`` () = 
+// TEST XLIFF_v1_2_Class
+[<Theory>]
+[<InlineData(0)>]
+[<InlineData(1)>]
+[<InlineData(2)>]
+[<InlineData(3)>]
+[<InlineData(4)>]
+[<InlineData(5)>]
+[<InlineData(6)>]
+[<InlineData(7)>]
+let ``XLIFF_v1_2_Class ReadXliffFile`` (index:int) =
     let instance = XLIFF_v1_2_Class()
-    let action = fun () -> instance.ThrowsException()
-    let ex : System.Exception = Assert.Throws<System.Exception>(action)
-    Assert.Equal("Throws exception!", ex.Message)
+    let action = fun () -> instance.ReadXliffFile(xliff_samples.[index])
+    action
+
+[<Theory>]
+[<InlineData(0)>]
+[<InlineData(1)>]
+[<InlineData(2)>]
+[<InlineData(3)>]
+[<InlineData(4)>]
+[<InlineData(5)>]
+[<InlineData(6)>]
+[<InlineData(7)>]
+let ``XLIFF_v1_2_Class ReadResxOrReswFile`` (index:int) =
+    let instance = XLIFF_v1_2_Class()
+    let action = fun () -> instance.ReadResxOrReswFile("file")
+    action
+
+[<Theory>]
+[<InlineData(0)>]
+[<InlineData(1)>]
+[<InlineData(2)>]
+[<InlineData(3)>]
+[<InlineData(4)>]
+[<InlineData(5)>]
+[<InlineData(6)>]
+[<InlineData(7)>]
+let ``XLIFF_v1_2_Class WriteXliffFile`` (index:int) =
+    let instance = XLIFF_v1_2_Class()
+    let action = fun () -> instance.WriteXliffFile("file")
+    let ex: System.Exception = Assert.Throws(action)
+    Assert.Equal("Not ready!", ex.Message)
+
+[<Theory>]
+[<InlineData(0)>]
+[<InlineData(1)>]
+[<InlineData(2)>]
+[<InlineData(3)>]
+[<InlineData(4)>]
+[<InlineData(5)>]
+[<InlineData(6)>]
+[<InlineData(7)>]
+let ``XLIFF_v1_2_Class WriteResxOrReswFile`` (index:int) =
+    let instance = XLIFF_v1_2_Class()
+    let action = fun () -> instance.WriteResxOrReswFile("file")
+    let ex: System.Exception = Assert.Throws(action)
+    Assert.Equal("Not ready!", ex.Message)
+
+[<Theory>]
+[<InlineData(0)>]
+[<InlineData(1)>]
+[<InlineData(2)>]
+[<InlineData(3)>]
+[<InlineData(4)>]
+[<InlineData(5)>]
+[<InlineData(6)>]
+[<InlineData(7)>]
+let ``XLIFF_v1_2_Class CheckXliffFile`` (index:int) =
+    let instance = XLIFF_v1_2_Class()
+    let action = fun () -> instance.CheckXliffFile("file")
+    let ex: System.Exception = Assert.Throws(action)
+    Assert.Equal("Not ready!", ex.Message)
+
+[<Theory>]
+[<InlineData(0)>]
+[<InlineData(1)>]
+[<InlineData(2)>]
+[<InlineData(3)>]
+[<InlineData(4)>]
+[<InlineData(5)>]
+[<InlineData(6)>]
+[<InlineData(7)>]
+let ``XLIFF_v1_2_Class CheckResxOrReswFile`` (index:int) =
+    let instance = XLIFF_v1_2_Class()
+    let action = fun () -> instance.CheckResxOrReswFile("file")
+    let ex: System.Exception = Assert.Throws(action)
+    Assert.Equal("Not ready!", ex.Message)
+
+
+
+
--- XLIFF_Library/Library.fs
+++ XLIFF_Library/Library.fs
@@ -3,14 +3,57 @@
 module XLIFF_v1_2 =
 
     type XLIFF_v1_2_Class() =
-        member this.Hello(name) =
-            printfn "Hello %s" name
 
-        member this.ThrowsException() =
-            raise (new System.Exception("Throws exception!")) |> ignore
+        member val ready : bool = false
+        member val document : System.Xml.XmlDocument
 
-        member this.ParseXliffFile(file) =
-            printfn "Parsing file %s" file
 
-    let hello name =
-        printfn "Hello %s" name
+        // READ
+
+        member this.ReadXliffFile(file : string) =
+            printfn "Reading XLIFF file %s" file
+            raise (new System.Exception("Not implemented")) |> ignore
+
+
+        member this.ReadResxOrReswFile(file) =
+            printfn "Reading RESX/W file %s" file
+            failwith "Not implemented" |> ignore
+
+
+        // WRITE
+
+        member this.WriteXliffFile(file) =
+            if not this.ready then
+                raise (new System.Exception("Not ready!")) |> ignore
+
+            printfn "Writing file %s" file
+            failwith "Not implemented" |> ignore
+
+
+        member this.WriteResxOrReswFile(file) =
+            if not this.ready then
+                raise (new System.Exception("Not ready!")) |> ignore
+
+            printfn "Writing file %s" file
+            failwith "Not implemented" |> ignore
+
+
+        // CHECK/VALIDATE/VERIFY/PARSE
+
+        member this.CheckXliffFile(file) =
+            if not this.ready then
+                raise (new System.Exception("Not ready!")) |> ignore
+
+            printfn "Checking file %s" file
+            failwith "Not implemented" |> ignore
+
+
+        member this.CheckResxOrReswFile(file) =
+            if not this.ready then
+                raise (new System.Exception("Not ready!")) |> ignore
+
+            printfn "Checking file %s" file
+            failwith "Not implemented" |> ignore
+
+
+
