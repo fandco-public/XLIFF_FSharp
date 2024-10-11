79930836f32036d5ce5193d2b85a485813e75105
Richard Freytag <rfreytag@freytag.us>
2024-10-11 12:12:09 -0400

    WIP - working XML & XLIFF.write(<file>) method and adding RESX and RESW classes.

---
+++ XLIFF_Library/Library_RESX_RESW.fs
@@ -0,0 +1,39 @@
+﻿namespace XLIFF_Library
+
+open System
+open System.IO
+open System.Reflection
+open System.Xml
+open System.Xml.Linq
+open System.Xml.Schema
+
+open Library_XML
+
+
+module RESX_RESW = 
+
+    type RESX_Class() = 
+
+        inherit XML_Class( None)
+
+        // READ
+
+        member this.ReadResxFile(xmlPath : string) : unit =
+            printfn "Reading XLIFF file %s\n\n" xmlPath
+            this.Read(xmlPath)
+
+        // CHECK/VALIDATE/VERIFY/PARSE
+
+        member this.CheckResxFile(file) =
+            //if not this.readyProperty then
+            //    raise (new System.Exception("Not ready!"))
+            this.Check(file)
+
+
+        // WRITE
+
+        member this.WriteResxFile(file) =
+            match this.Problems with
+            | Some _ -> raise (new System.Exception("Not ready!"))
+            | None -> this.Write(file)
+
---
+++ XLIFF_Library/Library_XLIFF.fs
@@ -0,0 +1,65 @@
+﻿namespace XLIFF_Library
+
+open System
+open System.IO
+open System.Reflection
+open System.Xml
+open System.Xml.Linq
+open System.Xml.Schema
+
+open Library_XML
+
+
+module XLIFF_v1_2 =
+
+
+    // XLIFF v1.2
+
+    type XLIFF_v1_2_Class() =
+        inherit XML_Class( Some [ @"Schemas\v1.2\xliff-core-1.2-transitional.xsd"; ])
+
+        // READ
+
+        member this.CheckXliffFile(file) =
+            match this.Problems with
+            | Some _ -> raise (new System.Exception("Not ready!"))
+            | None -> this.Check(file)
+
+        member this.ReadXliffFile(xmlPath : string) : unit =
+            printfn "Reading XLIFF file %s\n\n" xmlPath
+            this.Read(xmlPath)
+
+
+        member this.ReadResxOrReswFile(file) =
+            printfn "Reading RESX/W file %s" file
+            failwith "Not implemented"
+
+
+        // WRITE
+
+        member this.WriteXliffFile(file) =
+            match this.Problems with
+            | Some _ -> raise (new System.Exception("Not ready!"))
+            | None -> this.Write(file)
+
+
+        member this.WriteResxOrReswFile(file) =
+            printfn "Writing file %s" file
+            failwith "Not implemented"
+
+
+        // CHECK/VALIDATE/VERIFY/PARSE
+
+        member this.CheckXliffFile(file) =
+            printfn "Writing file %s" file
+            failwith "Not implemented"
+
+
+        member this.CheckResxOrReswFile(file) =
+            printfn "Writing file %s" file
+            failwith "Not implemented"
+
+
+
+
+
